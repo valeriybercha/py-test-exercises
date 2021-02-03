d43fff7010c614bf4f09cb0be40d223d8f386d28
Valeriy Bercha <valber.motorsport@gmail.com>
2021-02-03 23:33:43 +0200

    added edabit.py file

---
+++ edabit.py
@@ -0,0 +1,49 @@
+# Coding challenges solutions on Edabit platform
+
+
+# TASK - How Edabit Works
+
+def hello():
+	return "hello edabit.com"
+
+
+# TASK - Return the Sum of Two Numbers
+
+# Create a function that takes two numbers as arguments and return their sum.
+
+def addition(num1, num2):
+    return num1 + num2
+
+
+# TASK - Return the Next Number from the Integer Passed
+
+# Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+def addition(num):
+    return num + 1
+
+
+# TASK - Buggy Code (Part 1)
+
+# Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to 
+# get an idea of what the function should do.
+
+def cubes(a):
+	return a ** 3
+
+
+# TASK - Find the Perimeter of a Rectangle
+
+# Create a function that takes length and width and finds the perimeter of a rectangle.
+
+def find_perimeter(l, w):
+    return 2 * (l + w)
+
+
+# TASK - Convert Minutes into Seconds
+
+# Write a function that takes an integer minutes and converts it to seconds.
+
+def convert(min):
+    return min * 60
+
